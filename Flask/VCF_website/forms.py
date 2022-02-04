c889cc4787c0f1a799eaf87e0f305e2924569f2a
Mani <m.v.rudraraju@se21.qmul.ac.uk>
2022-02-04 13:03:42 +0000

    Fixed the search option bugs, should provide proper feedback

--- Flask/VCF_website/forms.py
+++ Flask/VCF_website/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired, Length, Email, Optional
+from wtforms.validators import DataRequired, Length, Email, Optional, ValidationError,InputRequired
 from VCF_website.models import VCF_db
+from markupsafe import Markup
+from wtforms.widgets.core import html_params
 
 class ContactForm(FlaskForm):
     name = StringField(label='Name', validators=[DataRequired()])
@@ -10,20 +12,50 @@ class ContactForm(FlaskForm):
     submit = SubmitField('Send')
 
 
+class CustomSelect:
+    """
+    Renders a select field allowing custom attributes for options.
+    Expects the field to be an iterable object of Option fields.
+    The render function accepts a dictionary of option ids ("{field_id}-{option_index}")
+    which contain a dictionary of attributes to be passed to the option.
+
+    Using this custom widget to disable CHR as an option so user cannot select
+    """
+
+    def __init__(self, multiple=False):
+        self.multiple = multiple
+
+    def __call__(self, field, option_attr=None, **kwargs):
+        if option_attr is None:
+            option_attr = {}
+        kwargs.setdefault("id", field.id)
+        if self.multiple:
+            kwargs["multiple"] = True
+        if "required" not in kwargs and "required" in getattr(field, "flags", []):
+            kwargs["required"] = True
+        html = ["<select %s>" % html_params(name=field.name, **kwargs)]
+        for option in field:
+            attr = option_attr.get(option.id, {})
+            html.append(option(**attr))
+        html.append("</select>")
+        return Markup("".join(html))
+
+
+
 class SearchPos(FlaskForm):
-    choices = [('CHR', 'CHR'),('1', '1'), ('22', '22')]
-    select = SelectField('Select Chromosome:', choices=choices)
-    start_pos = IntegerField('Starting position', validators=[DataRequired()])
-    end_pos = IntegerField('Ending position', validators=[Optional()])
-    submit = SubmitField('Search')
-    
+    choices = [("", "CHR"),('1', '1'), ('22', '22'),]
+    select = SelectField("Select Chromosome", choices=choices, validators=[DataRequired(message="Please select a chromosome")],widget=CustomSelect(),default="",)
+    start_pos = IntegerField("Starting position", validators=[DataRequired()])
+    end_pos = IntegerField("Ending position", validators=[Optional()])
+    submit = SubmitField("Search")
+
     
     
 class SearchRs(FlaskForm):
-    rs_val = StringField('rs value',validators=[DataRequired()])
-    rs_search = SubmitField('Search')
+    rs_val = StringField("rs value",validators=[DataRequired()])
+    rs_search = SubmitField("Search")
 
 
 class SearchGene(FlaskForm):
-    gene = StringField('Gene Name',validators=[DataRequired()])
-    gene_search = SubmitField('Search')
+    gene = StringField("Gene Name",validators=[DataRequired()])
+    gene_search = SubmitField("Search")
