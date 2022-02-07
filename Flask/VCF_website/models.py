d213c8afc0f08cb7fdd54285a494200c7952873a
Waleed Khan <81531255+AbuKamaal@users.noreply.github.com>
2022-02-07 20:25:41 +0000

    Updated models and routing
    Should now search properly within the database and output the query data. site.db is too large to upload on here so I will upload a copy of it on Microsoft teams. So far it only has data for the query table (rs, pos, ref, alt) but I'll upload an updated one with population data by the end of this week
    Note: for chromosome start and end position, the output looks messy, but still queries the database fine. Currently, rs value and genes can only handle single searches.

--- Flask/VCF_website/models.py
+++ Flask/VCF_website/models.py
@@ -1,13 +1,74 @@
 from VCF_website import db
 
 
-class VCF_db(db.Model):
+
+class query_search(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    Chrom = db.Column(db.String(20), unique=True, nullable=False)
+    chrom = db.Column(db.String(20), unique=False, nullable=False)
     rs_val = db.Column(db.String(20), unique=True, nullable=False)
-    Pos = db.Column(db.String(120), unique=True, nullable=False)
-    qual = db.Column(db.String(20), nullable=False, default='default.jpg')
+    pos = db.Column(db.String(120), unique=True, nullable=False)
+    gene_name = db.Column(db.String(120), unique=False)
+    ref_allele = db.Column(db.String(20), unique=False, nullable=False)
+    alt_allele = db.Column(db.String(20), unique=False, nullable=False)
 
     def __repr__(self):
-        return f"User('{self.id}', '{self.Chrom}', '{self.Pos}','{self.rs_val}')"
+        return 'chrom: {0} | RSID: {1} | Position: {2} | Reference: {3} | Alternate: {4} |'.format(self.chrom,self.rs_val,self.pos,self.ref_allele,self.alt_allele)
 
+
+class sample_info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sample_id = db.Column(db.String(20), nullable=False)
+    population_code = db.Column(db.String(20), nullable=False, unique=False)
+    superpopulation_code = db.Column(db.String(20), nullable=False, unique=False)
+
+class genotype_sample(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sample_id = db.Column(db.String(20), nullable=False, unique=False)
+    rs_val = db.Column(db.String(20), unique=True, nullable=False)
+    genotype = db.Column(db.String(20), nullable=False, unique=False)
+
+class snp_MXL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rs_val = db.Column(db.String(20), unique=True, nullable=False)
+    pos = db.Column(db.String(120), unique=True, nullable=False)
+    ref_allele = db.Column(db.String(20), unique=False, nullable=False)
+    alt_allele = db.Column(db.String(20), unique=False, nullable=False)
+    geno_freq = db.Column(db.String(20), unique=False, nullable=False)
+    allele_freq = db.Column(db.String(20), unique=False, nullable=False)
+
+class snp_GBR(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rs_val = db.Column(db.String(20), unique=True, nullable=False)
+    pos = db.Column(db.String(120), unique=True, nullable=False)
+    ref_allele = db.Column(db.String(20), unique=False, nullable=False)
+    alt_allele = db.Column(db.String(20), unique=False, nullable=False)
+    geno_freq = db.Column(db.String(20), unique=False, nullable=False)
+    allele_freq = db.Column(db.String(20), unique=False, nullable=False)
+
+class snp_PJL(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rs_val = db.Column(db.String(20), unique=True, nullable=False)
+    pos = db.Column(db.String(120), unique=True, nullable=False)
+    ref_allele = db.Column(db.String(20), unique=False, nullable=False)
+    alt_allele = db.Column(db.String(20), unique=False, nullable=False)
+    geno_freq = db.Column(db.String(20), unique=False, nullable=False)
+    allele_freq = db.Column(db.String(20), unique=False, nullable=False)
+
+class snp_JPT(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rs_val = db.Column(db.String(20), unique=True, nullable=False)
+    pos = db.Column(db.String(120), unique=True, nullable=False)
+    ref_allele = db.Column(db.String(20), unique=False, nullable=False)
+    alt_allele = db.Column(db.String(20), unique=False, nullable=False)
+    geno_freq = db.Column(db.String(20), unique=False, nullable=False)
+    allele_freq = db.Column(db.String(20), unique=False, nullable=False)
+
+class snp_YRI(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rs_val = db.Column(db.String(20), unique=True, nullable=False)
+    pos = db.Column(db.String(120), unique=True, nullable=False)
+    ref_allele = db.Column(db.String(20), unique=False, nullable=False)
+    alt_allele = db.Column(db.String(20), unique=False, nullable=False)
+    geno_freq = db.Column(db.String(20), unique=False, nullable=False)
+    allele_freq = db.Column(db.String(20), unique=False, nullable=False)
+    
