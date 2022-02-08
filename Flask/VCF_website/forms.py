1620fe2a64dfeae003d70b86f8841fd0aa83d797
Mani <m.v.rudraraju@se21.qmul.ac.uk>
2022-02-08 18:16:22 +0000

    Should return a none value if query comes up with empty and added validation check to make sure ending position is always greater than starting position

--- Flask/VCF_website/forms.py
+++ Flask/VCF_website/forms.py
@@ -41,12 +41,43 @@ class CustomSelect:
         return Markup("".join(html))
 
 
+class GreaterThan(object):
+    """
+    Compares the values of two fields.
+    :param fieldname:
+        The name of the other field to compare to.
+    :param message:
+        Error message to raise in case of a validation error. Can be
+        interpolated with `%(other_label)s` and `%(other_name)s` to provide a
+        more helpful error.
+    """
+    def __init__(self, fieldname, message=None):
+        self.fieldname = fieldname
+        self.message = message
+
+    def __call__(self, form, field):
+        try:
+            other = form[self.fieldname]
+        except KeyError:
+            raise ValidationError(field.gettext("Invalid field name '%s'.") % self.fieldname)
+        if field.data <= other.data:  #  --> Change to <= so it compares with start_pos
+            d = {
+                'other_label': hasattr(other, 'label') and other.label.text or self.fieldname,
+                'other_name': self.fieldname
+            }
+            message = self.message
+            if message is None:
+                message = field.gettext('Position must be larger than  starting position.')
+
+            raise ValidationError(message % d)
+
+
 
 class SearchPos(FlaskForm):
     choices = [("", "CHR"),('1', '1'), ('22', '22'),]
     select = SelectField("Select Chromosome", choices=choices, validators=[DataRequired(message="Please select a chromosome")],widget=CustomSelect(),default="",)
     start_pos = IntegerField("Starting position", validators=[DataRequired()])
-    end_pos = IntegerField("Ending position", validators=[Optional()])
+    end_pos = IntegerField("Ending position", validators=[Optional(),GreaterThan('start_pos')])
     submit = SubmitField("Search")
 
     
