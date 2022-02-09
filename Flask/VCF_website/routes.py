3993a3ad6c976cf0c0cc2435e9171ee9b35240ac
Waleed Khan <81531255+AbuKamaal@users.noreply.github.com>
2022-02-09 15:46:44 +0000

    Query search table output. Chromosome selection
    Updated the results.html and routes.py to output the query results as a table. May want to implement some page functionality to split up the results on multiple pages.

--- Flask/VCF_website/routes.py
+++ Flask/VCF_website/routes.py
@@ -36,13 +36,13 @@ def search():
             "start_pos" : form1.start_pos.data,
             "end_pos" : form1.end_pos.data
         }
-        return redirect(url_for('loading',variable=chromosome_position))
+        return redirect(url_for('results',search=chromosome_position))
     elif form2.rs_search.data and form2.validate_on_submit():
         form2_rs = f"'{form2.rs_val.data}'"
-        return redirect(url_for('loading',variable=form2_rs))
+        return redirect(url_for('results',search=form2_rs))
     elif form3.gene_search.data and form3.validate_on_submit():
         form3_gene = f"'{form3.gene.data}'"
-        return redirect(url_for('loading',variable=form3_gene))
+        return redirect(url_for('results',search=form3_gene))
     return render_template('search.html', title='About', form1=form1, form2=form2, form3=form3)
 
 def get_results(variable):
@@ -50,32 +50,56 @@ def get_results(variable):
     if isinstance(variable, dict):
         if variable["end_pos"] == None:
             results = query_search.query.filter(query_search.pos.like(int(variable["start_pos"]))).all()
-            return results
+            if len(results) == 0:
+                results = "None"
+            return render_template('results.html', title='Results', Results=results)
         else:
             results = query_search.query.filter(query_search.pos >= int(variable['start_pos'])).filter(query_search.pos <= int(variable['end_pos'])).all()
-            return results
+            if len(results) == 0:
+                results = "None"
+            return render_template('results.html', title='Results', Results=results)
     else:
         if variable.startswith('rs') == True:
             results = query_search.query.filter(query_search.rs_val.like(variable)).all()
-            return results
+            if len(results) == 0:
+                results = "None"
+            return render_template('results.html', title='Results', Results=results)
         else: 
             results = query_search.query.filter(query_search.gene_name.like(variable)).all()
-            return results
-        
+            if len(results) == 0:
+                results = "None"
+            return render_template('results.html', title='Results', Results=results)        
                 
 
 @app.route("/loading/<variable>",methods=["GET","POST"])
 def loading(variable):
     if request.method == "GET":
-        results = get_results(variable)
-        if len(results) == 0:
-            results = "None"
+        results = get_result(variable)
+        print(type(results))
+        #if len(results) == 0:
+        #    results = "None"
         return redirect(url_for("results",variable=results))
     return render_template('loading.html', title='Loading')
 
-@app.route("/results/<variable>",methods=['GET','POST'])
-def results(variable):
-    return render_template('results.html', title='Results', Results=variable)
+@app.route("/results/<search>",methods=['GET','POST'])
+def results(search):
+    if request.method == "GET":
+        variable = search
+        variable = ast.literal_eval(variable)
+        if isinstance(variable, dict):
+            if variable["end_pos"] == None:
+                results = query_search.query.filter(query_search.pos.like(variable['start_pos'])).filter(query_search.chrom == '{}'.format(variable["chr"])).all()
+                return render_template('results.html', title='Results', Results=results)
+            else:
+                results = query_search.query.filter(query_search.pos >= int(variable['start_pos'])).filter(query_search.pos <= int(variable['end_pos'])).filter(query_search.chrom == '{}'.format(variable['chr'])).all()
+                return render_template('results.html', title='Results', Results=results)
+        else:
+            if variable.startswith('rs') == True:
+                results = query_search.query.filter(query_search.rs_val.like(variable)).all()
+                return render_template('results.html', title='Results', Results=results)
+            else: 
+                results = query_search.query.filter(query_search.gene_name.like(variable)).all()
+                return render_template('results.html', title='Results', Results=results) 
 
     
 
