c8e5d21d94aaf9d57fda4178a0bec68c8a608895
Waleed Khan <81531255+AbuKamaal@users.noreply.github.com>
2022-02-20 10:21:13 +0000

    Improved query speed
    100k bases in 32 seconds (last I checked). Download the site.db from teams (should be 850mb). 
    For now, this query method has been set up for start and end positions. Will need to be updated for the other search parameters

--- Flask/VCF_website/models.py
+++ Flask/VCF_website/models.py
@@ -9,11 +9,17 @@ class query_search(db.Model):
     gene_name = db.Column(db.String(120), unique=False)
     ref_allele = db.Column(db.String(20), unique=False, nullable=False)
     alt_allele = db.Column(db.String(20), unique=False, nullable=False)
-    mxl = db.relationship('snp_MXL', backref='rs_val')
-    gbr = db.relationship('snp_GBR', backref='rs_val')
-    jpt = db.relationship('snp_JPT', backref='rs_val')
-    pjl = db.relationship('snp_PJL', backref='rs_val')
-    yri = db.relationship('snp_YRI', backref='rs_val')
+    mxl = db.relationship('snp_MXL', backref='rs_val', lazy='select')
+    gbr = db.relationship('snp_GBR', backref='rs_val', lazy='select')
+    jpt = db.relationship('snp_JPT', backref='rs_val', lazy='select')
+    pjl = db.relationship('snp_PJL', backref='rs_val', lazy='select')
+    yri = db.relationship('snp_YRI', backref='rs_val', lazy='select')
+    # mxl_array = db.relationship('MXL_array', backref='rs_val')
+    # gbr_array = db.relationship('GBR_array', backref='rs_val')
+    # jpt_array = db.relationship('JPT_array', backref='rs_val')
+    # pjl_array = db.relationship('PJL_array', backref='rs_val')
+    # yri_array = db.relationship('YRI_array', backref='rs_val')
+
     # def __init__(chrom, rs_val, pos, re_allele, alt_allele):
     #    self.chrom = chrom
     #    self.rs_val = rs_val
@@ -52,7 +58,6 @@ class snp_MXL(db.Model):
     allele_freq = db.Column(db.String(120), unique=False, nullable=False)
     rs_val_id = db.Column(db.String, db.ForeignKey('query_search.rs_val'))
     genotypes = db.Column(db.String, unique=False)
-
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name != 'id'}
 
@@ -63,7 +68,6 @@ class snp_GBR(db.Model):
     allele_freq = db.Column(db.String(120), unique=False, nullable=False)
     rs_val_id = db.Column(db.String, db.ForeignKey('query_search.rs_val'))
     genotypes = db.Column(db.String, unique=False)
-
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name != 'id'}
 
@@ -85,7 +89,6 @@ class snp_JPT(db.Model):
     allele_freq = db.Column(db.String(120), unique=False, nullable=False)
     rs_val_id = db.Column(db.String, db.ForeignKey('query_search.rs_val'))
     genotypes = db.Column(db.String, unique=False)
-
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name != 'id'}
 
@@ -96,6 +99,25 @@ class snp_YRI(db.Model):
     allele_freq = db.Column(db.String(120), unique=False, nullable=False)
     rs_val_id = db.Column(db.String, db.ForeignKey('query_search.rs_val'))
     genotypes = db.Column(db.String, unique=False)
-
     def to_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns if c.name != 'id'}
+
+# class MXL_array(db.Model):
+#     rs_val = db.Column(db.String,db.ForeignKey('query_search.rs_val'), primary_key=True)
+#     genotype = db.Column(db.String, unique=False)
+
+# class GBR_array(db.Model):
+#     rs_val = db.Column(db.String,db.ForeignKey('query_search.rs_val'), primary_key=True)
+#     genotype = db.Column(db.String, unique=False)
+
+# class JPT_array(db.Model):
+#     rs_val = db.Column(db.String,db.ForeignKey('query_search.rs_val'), primary_key=True)
+#     genotype = db.Column(db.String, unique=False)
+
+# class PJL_array(db.Model):
+#     rs_val = db.Column(db.String,db.ForeignKey('query_search.rs_val'), primary_key=True)
+#     genotype = db.Column(db.String, unique=False)
+
+# class YRI_array(db.Model):
+#     rs_val = db.Column(db.String,db.ForeignKey('query_search.rs_val'), primary_key=True)
+#     genotype = db.Column(db.String, unique=False)
