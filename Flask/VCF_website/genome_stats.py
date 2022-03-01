c8c77a3fd28f396d1557820769c5c9860814c294
Mani <93208890+Mani-varma1@users.noreply.github.com>
2022-03-01 16:14:11 +0000

    Update genome_stats.py

--- Flask/VCF_website/genome_stats.py
+++ Flask/VCF_website/genome_stats.py
@@ -1,4 +1,5 @@
 # Stats script
+from tracemalloc import start, stop
 import plotly.graph_objects as go
 import plotly
 import json
@@ -28,11 +29,11 @@ def Homozygosity(freq_data):
 # would need to be run for each population if multiple populations are selected. 
 
 
-def nuc_div(pop):
+def nuc_div(pop,pos):
     pop_gt = np.array(pop)
     pop = allel.GenotypeArray(pop_gt)
-    h = pop.to_haplotypes()
-    pi= allel.haplotype_diversity(h=h)
+    ac = pop.count_alleles()
+    pi= allel.sequence_diversity(pos=pos, ac=ac)
     
     return round(pi,3)
 
@@ -52,7 +53,7 @@ def tajima_d(pop, pos):
     pop_gt = np.array(pop)
     pop = allel.GenotypeArray(pop_gt)
     ac = pop.count_alleles()
-    D= allel.tajima_d(ac=ac, pos=pos)
+    D= allel.tajima_d(ac=ac, pos=pos )
     
     return round(D,3)
 
@@ -73,7 +74,9 @@ def hudson_fst(pop1,pop2):
 
 
 
-
+""" Get main stats bundles other basic statistics into a single functiona and also 
+    checks for the user input before calculation
+"""
 def get_main_stats(pop,freq_data,pos,stats):
     if 'Homozygosity' in stats:
         homo = Homozygosity(freq_data)
@@ -82,9 +85,15 @@ def get_main_stats(pop,freq_data,pos,stats):
 
 
     if 'Nucleotide Diversity' in stats:
-        nd = nuc_div(pop)
+        nd = nuc_div(pop,pos=pos)
     else:
         nd = 'Not calculated'
+
+    
+    if 'Haplotype Diversity' in stats:
+        hap_div = haplotype_div(pop)
+    else:
+        hap_div = 'Not calculated'
 
 
     if 'Tajimas D' in stats:
@@ -93,16 +102,13 @@ def get_main_stats(pop,freq_data,pos,stats):
         taj_d = 'Not calculated'
 
 
-    if 'Haplotype Diversity' in stats:
-        hap_div = haplotype_div(pop)
-    else:
-        hap_div = 'Not calculated'
     
     return homo,nd,hap_div,taj_d
 
 
 
-
+""" First get the genotype data in a dictionary format with their populations code as the key.
+performs permutations to create all pairwise comparisions in a tuple e.g.('GBR','JPT'). For each permutation, it calculates the fst by using the items in tuple to only call the genotype data that is specific to the population  """
 def get_fstat(paris,gt_dict):
     combos = itertools.permutations(paris,2)
     lst = []
@@ -119,6 +125,7 @@ def get_fstat(paris,gt_dict):
 
 
 
+""" For calculating the stats using scikit allel using genotype data and combining all the individuals into a single super population"""
 def overall_stats_gtd(all_pops_array):
     for i,ele in enumerate(all_pops_array):
         ele = np.array(ele)
@@ -128,6 +135,8 @@ def overall_stats_gtd(all_pops_array):
             all_pop_arr = np.hstack((all_pop_arr,ele))
     return all_pop_arr
 
+    
+""" For calculating observed Homozygosity using count data and combining all the individuals into a single super population"""
 def overall_stats_cts(all_pop):
     full_pop_data = []
     if len(full_pop_data) == 0:
@@ -150,13 +159,13 @@ def overall_stats_cts(all_pop):
 
     
 
-def avg_win(pos,size):
-    # windows = allel.moving_statistic(pos, statistic= lambda v: [v[0], v[-1]], size=size)
-    x = np.asarray(pos).mean(axis=1)
-    return x
+
 
     
-    
+
+############################################################################################################################################
+######################################################    WINDOWED STATS ###################################################################
+############################################################################################################################################
     
 
 
@@ -168,7 +177,8 @@ def win_nuc_div(positions,pop,bin_size=100,step_size=None):
     pop = allel.GenotypeArray(pop_gt)
     ac = pop.count_alleles()
     win_pi, windows, n_bases, counts = allel.windowed_diversity(pos,ac,size=bin_size,step=step_size)
-    return win_pi,windows
+    return win_pi
+
 
 
 def win_tajima_d(positions,pop,bin_size=100,step_size=None):
@@ -206,16 +216,38 @@ def win_hudson_fst(positions,pop1,pop2,bin_size=100,step_size=None):
     pop2 = allel.GenotypeArray(pop2_gt)
     ac1 = pop1.count_alleles()
     ac2 = pop2.count_alleles()
-    win_fst, windows, counts = allel.windowed_hudson_fst(positions,ac1, ac2,bin_size=bin_size,step_size=step_size)
+
+    win_fst, windows, counts = allel.windowed_hudson_fst(positions,ac1, ac2,size=bin_size,step=step_size)
+
     return win_fst
 
 
 
+""" Similar to fstat we are doing permutations and inthis case also providing the position and window sizes to be used for windowed stats"""
+def get_win_fstat(paris,gt_dict,pos,bin_size,step_size):
+    combos = itertools.permutations(paris,2)
+    lst = []
+    for i in combos:
+        pair = f'{i[0]}:{i[1]}'
+        fst= win_hudson_fst(positions=pos,pop1 = gt_dict[i[0]], pop2= gt_dict[i[1]],bin_size=bin_size,step_size=step_size)
+        lst.append([pair,fst])
+    
+    return lst
 
 
-"""
-Creating clots
-"""
+
+
+
+############################################################################################################################################
+########################################################  Creating Plots ###################################################################
+############################################################################################################################################
+
+
+def avg_win(pos,size,step):
+    windows = allel.position_windows(pos=pos, size=size, step=step, start=None, stop=None)
+    x = np.asarray(windows).mean(axis=1)
+    return x
+
 
 def plot_win_taj_d(TD, position, num_pops):
 
@@ -403,3 +435,48 @@ def plot_nuc_div(ND, position,num_pops):
 
 
     return graph3JSON,graph4JSON
+
+
+
+def plot_win_FST(pop_FST, position):
+    """ Get the """
+    fig_7 = go.Figure()
+    buttons = []
+    i = 0
+
+    # iterating through dictionary and adding each population
+    for key,value in pop_FST.items():
+        obj = go.Scatter(name=key, x=position, y=value)
+        fig_7.add_trace(obj)
+
+    #args is a list of booleans that tells the buttons which trace to show on click
+        args = [False] * len(pop_FST)
+        args[i] = True
+
+    #create button object for each pop
+        button = dict(label=key,
+                        method="update",
+                        args=[{"visible": args}])
+
+    #add button to list
+        buttons.append(button)
+        i += 1
+
+    #add buttons
+    fig_7.update_layout(
+        updatemenus=[
+            dict(
+                type="dropdown",
+                direction="down",
+                buttons=buttons)
+        ])
+    #add axis names
+    fig_7.update_layout(
+        xaxis_title='Position (Base pairs)',
+        yaxis_title="FST")
+
+    fstJSON = json.dumps(fig_7, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+
+    return fstJSON
